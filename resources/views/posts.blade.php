d4b8eecb3576f90edb10b5b005d8b444d4ddbda2
MD rouf <rauf.official2020m>
2023-03-16 15:35:54 +0600

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-
-<title>My Blog</title>
-<link rel="stylesheet" href="style.css">
-<body>
+<x-layout>
     @foreach ($posts as $post )
-
-
-    <article>
-       <h1><a href="posts/{{$post->slug}}">{{$post->title}}</a></h1>
-       <p> By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in
-        <a href="/categories/{{$post->category->slug}}">
-            {{$post->category->name}}</a>
-    </p>
-       <p>{{$post->excerpt}}</p>
-    </article>
+       <article>
+        <h1>
+            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+        </h1>
+        <div>
+            {{ $post->excerpt }}
+        </div>
+        </article>
     @endforeach
-</body>
+</x-layout>
